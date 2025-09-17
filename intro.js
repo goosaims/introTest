b641119c68392603062a7881e7b5c0011d714f0f
goosaims <amesgusa@outlook.com>
2025-09-17 13:48:00 -0600

    added index html and intro.js

---
+++ intro.js
@@ -0,0 +1,22 @@
+// //console.log(cheese);
+
+// let cheese = 234.45;
+// console.log('Hi');
+
+// cheeseCost();
+
+// function cheeseCost() {
+//     const quantity = 3;
+//     let costTotal = quantity/cheese;
+//     console.log(costTotal);
+// }
+
+
+let ice_cream = "toppings";
+const toppings = 3;
+let friends = true;
+let teachers = null;
+
+alert("404 ERROR!!!      please refresh page")
+console.log(alert)
+//school comment test hehe
